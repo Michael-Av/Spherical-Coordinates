b6735fb336b2c66070039ac6b5b94fff1ada7f6b
Julien Diamond <jdiamond50@stuy.edu>
2025-03-30 16:58:12 -0400

    found all sides of astronimcal triangle

--- calculateCoordinates.py
+++ calculateCoordinates.py
@@ -13,10 +13,21 @@ altitude = PI / 4;
 # observer location
 latitude = PI / 6;
 
+# define vectors
 zenithVec = np.array([0, 0, 1])
-poleVec = np.array([1, 0, np.sin(latitude)])
+poleVec = np.array([1, 0, np.tan(latitude)])
 objectVec = np.array([np.cos(azimuth), -1 * np.sin(azimuth), np.tan(altitude)])
+
+# normalize vectors
+zenithVec = zenithVec / np.linalg.norm(zenithVec)
+poleVec = poleVec / np.linalg.norm(poleVec)
+objectVec = objectVec / np.linalg.norm(objectVec)
 
 print("zenith: ", zenithVec)
 print("pole: ", poleVec)
 print("object: ", objectVec)
+
+# sides of astronomical triangle
+OP = np.arccos(poleVec.dot(objectVec))
+PZ = PI / 2 - latitude
+ZO = PI / 2 - altitude
