ba8e9484e5089819e5b6b1d170082900f7c804bd
Julien Diamond <jdiamond50@stuy.edu>
2025-04-03 11:01:35 -0400

    mikltest

---
+++ calcCoordTestFileToTestCalcCoord.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+latitude=1
+longitude=1
+altitude=1
+azimuth=1
+siderealTime=1
+
+python3 calculateCoordinates.py azimuth altitude latitude siderealTime
--- calculateCoordinates.py
+++ calculateCoordinates.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+import math
 
 PI = np.pi
 
@@ -6,37 +8,46 @@ PI = np.pi
 # +y = west
 # +z = up (towards zenith)
 
-# horizontal coordinates
-azimuth = PI / 3
-altitude = PI / 4
+# Observer coordinates
+latitude=89
+siderealTime=0
 
-# observer details
-latitude = PI / 6
-siderealTime = PI
+# Object coordinates
+azimuth=0
+altitude=89
 
-# define vectors
-zenithVec = np.array([0, 0, 1])
-poleVec = np.array([1, 0, np.tan(latitude)])
-objectVec = np.array([np.cos(azimuth), -1 * np.sin(azimuth), np.tan(altitude)])
+def calcCoords(azimuth, altitude, latitude, siderealTime):
+	# define vectors
+	zenithVec = np.array([0, 0, 1])
+	poleVec = np.array([1, 0, np.tan(latitude)])
+	objectVec = np.array([np.cos(azimuth), -1 * np.sin(azimuth), np.tan(altitude)])
 
-# normalize vectors
-zenithVec = zenithVec / np.linalg.norm(zenithVec)
-poleVec = poleVec / np.linalg.norm(poleVec)
-objectVec = objectVec / np.linalg.norm(objectVec)
+	# normalize vectors
+	zenithVec = zenithVec / np.linalg.norm(zenithVec)
+	poleVec = poleVec / np.linalg.norm(poleVec)
+	objectVec = objectVec / np.linalg.norm(objectVec)
 
-#print("zenith: ", zenithVec)
-#print("pole: ", poleVec)
-#print("object: ", objectVec)
+	#print(poleVec)
+	#print(objectVec)
+	#print(poleVec.dot(objectVec))
+	dotProduct = math.trunc(poleVec.dot(objectVec)*1000) / 1000.0
+	#print(dotProduct)
+	#print("zenith: ", zenithVec)
+	#print("pole: ", poleVec)
+	#print("object: ", objectVec)
 
-# sides of astronomical triangle
-OP = np.arccos(poleVec.dot(objectVec))
-PZ = PI / 2 - latitude
-ZO = PI / 2 - altitude
+	# sides of astronomical triangle
+	OP = np.arccos(dotProduct)
+	PZ = PI / 2 - latitude
+	ZO = PI / 2 - altitude
 
-# le mathimatique
-declination = PI / 2 - OP
-hourAngle = np.arccos((np.cos(ZO) - np.cos(OP) * np.cos(PZ)) / (np.sin(OP) * np.sin(PZ)))
-rightAscension = siderealTime - hourAngle
+	# le mathimatique
+	declination = PI / 2 - OP
+	hourAngle = np.arccos((np.cos(ZO) - np.cos(OP) * np.cos(PZ)) / (np.sin(OP) * np.sin(PZ)))
+	rightAscension = siderealTime - hourAngle
 
-print("declination = ", declination)
-print("rightAscension = ", rightAscension)
+	print("declination = ", declination)
+	print("rightAscension = ", rightAscension)
+
+calcCoords(azimuth, altitude, latitude, siderealTime)
+
