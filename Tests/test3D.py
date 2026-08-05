892943bc789c2ff2d294ad3f3abefae4f5581c1e
Julien Diamond <juliendiamond@MacBook-Pro-2.local>
2026-08-05 16:34:35 -0400

    altitude and azimuth lines drawn

--- Tests/test3D.py
+++ Tests/test3D.py
@@ -1,23 +1,48 @@
 import pyvista as pv
 import numpy as np
 
+# background sphere
 background_sphere = pv.Sphere(radius=1)
 
+# axes
+phi_ticks = np.radians(np.arange(0,181,10))
+theta_ticks = np.radians(np.arange(0,360,10))
+
+phi_points, theta_points = np.meshgrid(phi_ticks, theta_ticks, indexing='ij')
+
+phi_points = phi_points.ravel()
+theta_points = theta_points.ravel()
+
+x_axis_points = 0.95 * np.sin(theta_points) * np.cos(phi_points)
+y_axis_points = 0.95 * np.sin(theta_points) * np.sin(phi_points)
+z_axis_points = 0.95 * np.cos(theta_points)
+
+axis_points = np.column_stack((x_axis_points,y_axis_points,z_axis_points))
+
+connection_indices = []
+for i in range(1, len(axis_points)+1):
+    connection_indices.append([i,i-1]) # connects altitude lines
+    if i >= 36:
+        connection_indices.append([i,i-36]) # connects azimuth lines
+
+axes = pv.PolyData(axis_points, lines=pv.CellArray.from_regular_cells(connection_indices))
+
+# stars
 phi_vals = np.random.rand(100) * 2*np.pi
 theta_vals = np.random.rand(100) * 2*np.pi
 
-x = 0.9 * np.sin(theta_vals) * np.cos(phi_vals)
-y = 0.9 * np.sin(theta_vals) * np.sin(phi_vals)
-z = 0.9 * np.cos(theta_vals)
+star_x = 0.9 * np.sin(theta_vals) * np.cos(phi_vals)
+star_y = 0.9 * np.sin(theta_vals) * np.sin(phi_vals)
+star_z = 0.9 * np.cos(theta_vals)
 
-star_positions = np.column_stack((x,y,z))
+star_positions = np.column_stack((star_x,star_y,star_z))
 
 stars = pv.PolyData(star_positions)
 
 pl = pv.Plotter()
 pl.add_mesh(background_sphere, color='black', show_edges=True)
+pl.add_mesh(axes, color='red')
 pl.add_mesh(stars, color='white')
-
 
 # 2. Fix the initial position (standing in one spot)
 EYE_POS = np.array([0.0, 0.0, 0.0])
@@ -51,14 +76,14 @@ def on_mouse_move(iren, event):
     dy = curr_pos[1] - state["last_pos"][1]
     state["last_pos"] = curr_pos
     
-    state["yaw"] -= dx * 0.4
-    state["pitch"] = np.clip(state["pitch"] + (dy * 0.4), -85, 85)
+    state["yaw"] -= dx * 0.05
+    state["pitch"] = np.clip(state["pitch"] + (dy * 0.05), -85, 85)
     
     rad_y, rad_p = np.radians(state["yaw"]), np.radians(state["pitch"])
     look_dir = np.array([
         np.cos(rad_p) * np.sin(rad_y),
         np.cos(rad_p) * np.cos(rad_y),
-        np.sin(rad_p)
+        -np.sin(rad_p)
     ])
     
     # Force coordinates to lock, change look direction
